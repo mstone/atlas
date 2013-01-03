85bd968ebb6d6ba18b1702e78471ce2cfc67f5d4
Michael Stone <mistone@akamai.com>
2013-01-03 14:57:24 -0500

    Add more questions.
    Change-Id: I3b431c65e36c7808b8f1c623be6e613ffaa994c3

--- src/persist/persist.go
+++ src/persist/persist.go
@@ -19,9 +19,149 @@ func (self *PersistMem) GetAllProfiles() ([]*entity.Profile, error) {
 		Version: entity.Version{"pace", 1, 0, 0},
 		Questions: []*entity.Question{
 			&entity.Question{
-				Version:     entity.Version{"who", 1, 0, 0},
-				Text:        "Who?",
-				Help:        "...'s on first?",
+				Version:     entity.Version{"agora-pages", 1, 0, 0},
+				Text:        "Which agora pages describe your product?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"bart-releases", 1, 0, 0},
+				Text:        "Which BART releases are you using?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"billing-method", 1, 0, 0},
+				Text:        "What's your billing method?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"code-lines", 1, 0, 0},
+				Text:        "What are your code lines?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"component-names", 1, 0, 0},
+				Text:        "What are your components called?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"features", 1, 0, 0},
+				Text:        "What features does your product build on?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"bugzilla-tickets", 1, 0, 0},
+				Text:        "What CRs does your launch close?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"deployed-networks", 1, 0, 0},
+				Text:        "What networks will you be deploying to?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"engineering-schedule-contact", 1, 0, 0},
+				Text:        "Who can speak authoritatively about your engineering schedule?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"etherpad-links", 1, 0, 0},
+				Text:        "Which etherpad should we use to collaborate on writing up your review?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"implementation-contact", 1, 0, 0},
+				Text:        "Who can speak authoritatively about your product's technical implementation details?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"implementation-contact", 1, 0, 0},
+				Text:        "Who can speak authoritatively about your product's technical implementation details?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"secarch-review-link", 1, 0, 0},
+				Text:        "Which SecArch review covers your product?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"olympus-tickets", 1, 0, 0},
+				Text:        "Which Olympus KFs does your product address?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"module-design-link", 1, 0, 0},
+				Text:        "Where is your module design document?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"product-contacts", 1, 0, 0},
+				Text:        "Who can speak authoritatively about your product's strategy, packaging, etc.",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"program-manager", 1, 0, 0},
+				Text:        "Who is program-managing your product?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"secarch-advisor", 1, 0, 0},
+				Text:        "Who from SecArch is advising you?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"secarch-reviewer", 1, 0, 0},
+				Text:        "Who from SecArch is reviewing your product's security considerations?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"technical-design-contact", 1, 0, 0},
+				Text:        "Who can speak authoritatively about your product's technical architecture and design?",
+				Help:        "",
+				AnswerType:  entity.ANSWER_TYPE_TEXT,
+				DisplayHint: "",
+			},
+			&entity.Question{
+				Version:     entity.Version{"jira-tickets", 1, 0, 0},
+				Text:        "What Jira tickets does this launch or commit address?",
+				Help:        "",
 				AnswerType:  entity.ANSWER_TYPE_TEXT,
 				DisplayHint: "",
 			},
@@ -44,19 +184,11 @@ func (self *PersistMem) GetProfileById(version entity.Version) (*entity.Profile,
 }
 
 func (self *PersistMem) GetAllQuestions() ([]*entity.Question, error) {
-	v := make([]*entity.Question, 1)
-	v[0] = &entity.Question{
-		Version:     entity.Version{"who", 1, 0, 0},
-		Text:        "Who?",
-		Help:        "...'s on first?",
-		AnswerType:  entity.ANSWER_TYPE_TEXT,
-		DisplayHint: "",
-	}
-	return v, nil
+	return nil, errors.New("PersistMem.GetAllQuestions() not implemented")
 }
 
 func (self *PersistMem) GetQuestionById(version entity.Version) (*entity.Question, error) {
-	return nil, errors.New("PersistMem.GetQuestionById not implemented")
+	return nil, errors.New("PersistMem.GetQuestionById() not implemented")
 }
 
 func (self *PersistMem) GetAllReviews() ([]*entity.Review, error) {
